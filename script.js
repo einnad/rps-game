0a078c64c82ac9eae73e53dffca912f8d1aa1e95
einnad <146109295+einnad@users.noreply.github.com>
2024-01-19 17:15:49 +0000

    Fix play round function

--- script.js
+++ script.js
@@ -1,112 +1,57 @@
-/*user types in r p s
+/*user clicks r p s
 computer generates a random result
 int displays winner or loser message
 rerun */
 
-/* let choices = ["rock", "paper", "scissors"]; */
 let computerChoice;
 let playerChoice;
-let playerScore = 0;
-let computerScore = 0;
+// for full game
+// let playerScore = 0;
+// let computerScore = 0;
 
 const btnRock = document.querySelector(".rock");
 const btnPaper = document.querySelector(".paper");
 const btnScissors = document.querySelector(".scissors");
+const result = document.querySelector(".result");
+const compChoice = document.querySelector(".comp");
+
+const getResult = function (playerChoice, computerChoice) {
+  compChoice.textContent = computerChoice;
+  if (playerChoice === computerChoice) {
+    result.textContent = "It's a tie!";
+  } else if (
+    (playerChoice === "Rock" && computerChoice === "Scissors") ||
+    (playerChoice === "Paper" && computerChoice === "Rock") ||
+    (playerChoice === "Scissors" && computerChoice === "Paper")
+  ) {
+    result.textContent = "Player wins!";
+  } else result.textContent = "Computer wins!";
+};
 
 btnRock.addEventListener("click", function () {
-  playerChoice = "rock";
-  console.log(playerChoice);
+  playerChoice = "Rock";
+  computerChoice = getComputerChoice();
+  getResult(playerChoice, computerChoice);
 });
 
 btnPaper.addEventListener("click", function () {
-  playerChoice = "paper";
-  console.log(playerChoice);
+  playerChoice = "Paper";
+  computerChoice = getComputerChoice();
+  getResult(playerChoice, computerChoice);
 });
 
 btnScissors.addEventListener("click", function () {
-  playerChoice = "scissors";
-  console.log(playerChoice);
+  playerChoice = "Scissors";
+  computerChoice = getComputerChoice();
+  getResult(playerChoice, computerChoice);
 });
 
 function getComputerChoice() {
   const randomNumber = Math.floor(Math.random() * 3) + 1;
   if (randomNumber === 1) {
-    computerChoice = "rock";
+    computerChoice = "Rock";
   } else if (randomNumber === 2) {
-    computerChoice = "paper";
-  } else computerChoice = "scissors";
+    computerChoice = "Paper";
+  } else computerChoice = "Scissors";
   return computerChoice;
 }
-
-computerChoice = getComputerChoice();
-
-/* if rock and scissors
-if rock and paper
-if scissors and paper
-and reverse
-if rock and rock
-if paper and paper
-if scissors and scissors */
-
-// function playRound(playerChoice, computerChoice) {
-//   if (playerChoice === computerChoice) {
-//     return "Draw!";
-//   }
-//   if (playerChoice === "rock" && computerChoice === "scissors") {
-//     return "You win!";
-//   }
-//   if (playerChoice === "paper" && computerChoice === "rock") {
-//     return "You win!";
-//   }
-//   if (playerChoice === "scissors" && computerChoice === "paper") {
-//     return "You win!";
-//   } else return "You lose. Try again.";
-// }
-
-// console.log(playRound(playerChoice, computerChoice));
-
-// function game() {
-//   /* if a win, 'score' increments for player but not computer
-//        if lose, 'score' increments for computer but not player.
-//        console.log result each round
-//        the 'loop' ends after five round and if player score > computer score then player wins.
-//        if computer score > player score then player loses.
-//        new game prompt? button */
-//   for (let i = 0; i < 5; i++) {
-//     playRound(playerChoice, computerChoice);
-//     if (playerChoice === "rock" && computerChoice === "scissors") {
-//       playerScore++;
-//       console.log(playerScore, computerScore);
-//     }
-//     if (playerChoice === "paper" && computerChoice === "rock") {
-//       playerScore++;
-//       console.log(playerScore, computerScore);
-//     }
-//     if (playerChoice === "scissors" && computerChoice === "paper") {
-//       playerScore++;
-//       console.log(playerScore, computerScore);
-//     }
-//     if (playerChoice === "rock" && computerChoice === "paper") {
-//       computerScore++;
-//       console.log(playerScore, computerScore);
-//     }
-//     if (playerChoice === "paper" && computerChoice === "scissors") {
-//       computerScore++;
-//       console.log(playerScore, computerScore);
-//     }
-//     if (playerChoice === "scissors" && computerChoice === "rock") {
-//       computerScore++;
-//       console.log(playerScore, computerScore);
-//     } else console.log(playerScore, computerScore);
-//     getComputerChoice();
-//     computerChoice = getComputerChoice();
-//     console.log(playerChoice, computerChoice);
-//   }
-//   if (playerScore > computerScore) {
-//     return "You won the game!";
-//   } else if ((playerScore = computerScore)) {
-//     return "The game ended in a draw. I'll keep the gold star.";
-//   } else return "You lost the game. Try again?";
-// }
-
-// console.log(game());
